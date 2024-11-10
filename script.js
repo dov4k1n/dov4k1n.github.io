0745de62f3c0f5f9b4608d84d3349643f2167443
Ayzat Rizatdinov <ddov4k1n@gmail.com>
2024-11-10 04:47:25 +0300

    deploy

---
+++ script.js
@@ -0,0 +1,38 @@
+const footer = document.createElement('footer');
+footer.innerHTML = `
+    <footer>
+  <div class="social-networks">
+    <div class="social-networks-item">
+      <a href="https://www.t.me/tatar_app" class="social-networks-item-logo"><img src="/res/tg_icon.svg" class="logo">Telegram</img></a>
+      Подписывайтесь на @tatar_app в Telegram
+    </div>
+    <div class="social-networks-item">
+      <a href="https://www.youtube.com/@tatarcham" class="social-networks-item-logo"><img src="/res/yt_icon.webp" class="logo">YouTube</img></a>
+      Подписывайтесь на @tatarcham на YouTube
+    </div>
+    <div class="social-networks-item">
+      <a href="https://www.vk.com/tatarcham" class="social-networks-item-logo"><img src=0/"res/vk_icon.svg" class="logo">VK</img></a>
+      Подписывайтесь на @tatarcham в VK
+    </div>
+  </div>
+
+  <div class="ios-waitlist">
+    <div class="social-networks-item">
+      <a href="https://www.t.me/tatar_app" class="social-networks-item-logo"><img src="/res/android_icon.svg" class="logo">Android</img></a>
+      Скачайте приложение на Android
+    </div>
+
+    <div class="social-networks-item">
+      <a href="https://www.t.me/tatarcham_ios" class="social-networks-item-logo"><img src="/res/apple_icon.svg" class="logo">iOS</img></a>
+      Лист ожидания приложения для iOS
+    </div>
+  </div>
+
+  <div class="developer">
+    <a href="/index.html"><img src="/res/logo.svg"></a>
+    <p class="label-small">Copyright © 2024 tatarcham inc. All rights reserved.</p>
+    <span class="label-small">Developed by: <a href="https://t.me/dov4k1n" class="link">@dov4k1n</a></span>
+  </div>
+</footer>
+`;
+document.body.appendChild(footer);
