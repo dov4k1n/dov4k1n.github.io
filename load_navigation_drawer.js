584d0f0a8f711a3d4a7d4ae78cb5c15bbc4ee66a
Ayzat Rizatdinov <ddov4k1n@gmail.com>
2024-12-11 01:30:30 +0300

    -

--- load_navigation_drawer.js
+++ load_navigation_drawer.js
@@ -5,6 +5,8 @@ fetch('../html/navigation_drawer.html')
     navigation_drawer.innerHTML = data;
     document.body.appendChild(navigation_drawer);
 
+    // Active state handling:
+
     const currentURL = window.location.pathname;
     const menuItems = navigation_drawer.querySelectorAll('.navigation-drawer-list-item a');
 
@@ -18,16 +20,3 @@ fetch('../html/navigation_drawer.html')
   .catch(error => {
     console.error('Error loading navigation_drawer.html:', error);
   });
-
-// Active state handling:
-
-const navigationList = document.getElementById('navigation-drawer-list');
-const listItems = navigationList.getElementsByClassName('navigation-drawer-list-item');
-
-listItems.forEach(item => {
-  item.addEventListener('click', function(event) {
-    listItems.forEach(listItem => listItem.classList.remove('active'));
-
-    this.classList.add('active');
-  });
-});
